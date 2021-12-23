ccad49bb4477f83d7835a95d896d4592651f6f78
Marie Gurke <marie.gurke@mfn.berlin>
2021-12-23 10:27:02 +0100

    Utils opt

--- utils/RaxML_phylogenies.sh
+++ utils/RaxML_phylogenies.sh
@@ -42,9 +42,11 @@ fi
 
 
 cd $outfolder
-
+c=0
 for fasta in $infolder*.fasta; do
    ((i=i%N)); ((i++==0)) && wait
    sed -i 's/://g' $fasta &
-   raxmlHPC-PTHREADS -m GTRGAMMA -T $threads -p 12345 -s $fasta -N $MLsearches -n $(basename $fasta .fasta) &
+   raxmlHPC-PTHREADS -m GTRGAMMA -T $threads -p 12345 -s $fasta -N $MLsearches -n $(basename $fasta .fasta) > /dev/null &
+   c=$((c+1))
+   echo "$c fasta files processed."
 done
